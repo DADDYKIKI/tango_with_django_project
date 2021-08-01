feeef8278657cf8b77a6d8a9658dd6bb97c76a22
DADDYKIKI <88001517+DADDYKIKI@users.noreply.github.com>
2021-08-01 19:58:57 +0100

    Add files via upload

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf.urls import url
 from django.urls import path
 from django.urls import include
 from rango import views
@@ -22,6 +23,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('category/<slug:category_name_slug/', views.show_category, name='show_category'),
     path('rango/', include('rango.urls')),
     path('admin/', admin.site.urls),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
